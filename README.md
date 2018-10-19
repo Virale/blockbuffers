c202c214f43539e4e6664762a027238ef3e1efb3
ian <me@iany.me>
2018-10-19 11:20:48 +0800

    feat: setup the project
    The project contains two part:
    
    - A python library and executable to generate code.
    - A rust library supporting generated Rust code. It contains integrated test
    to test the generated code.

---
+++ README.md
@@ -0,0 +1,5 @@
+# fbg
+
+Flatbuffers Generator
+
+[Documentation](https://nervosfoundation.github.io/fbg/doc/fbg/index.html)
---
+++ tests/io_tests.rs
@@ -0,0 +1,20 @@
+extern crate fbg;
+extern crate flatbuffers;
+
+pub mod common;
+
+use common::example_generated::example::{get_root_as_example, Example};
+use flatbuffers::FlatBufferBuilder;
+
+#[test]
+fn io_happy_pass() {
+    let (buf, loc) = {
+        let mut builder = FlatBufferBuilder::new_with_capacity(1024);
+        let ex = Example::create(&mut builder, &Default::default());
+        builder.finish(ex, None);
+        builder.collapse()
+    };
+
+    let ex = get_root_as_example(&buf[loc..]);
+    assert_eq!(0, ex.version());
+}
